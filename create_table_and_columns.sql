3781d63bf4a69303baa5e98c9639a3063b6d512a
guihh <g.herault@outlook.fr>
2018-07-22 10:13:55 +0200

    Create create_table_and_columns.sql

---
+++ create_table_and_columns.sql
@@ -0,0 +1,11 @@
+CREATE TABLE public.bano
+(
+    "id" varchar,
+    "numero" varchar,
+    "voie" varchar,
+    "code_post" char(5),
+    "nom_comm" varchar,
+    "source " varchar,
+    "lat" real,
+    "lon" real,
+);
